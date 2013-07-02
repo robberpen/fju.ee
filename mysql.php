341b1dc801df3027a5fd2c3c76f518228e5d1e46
peter <peter_pan@cameo.com.tw>
2013-07-03 03:59:24 +0800

    backup

--- mysql.php
+++ mysql.php
@@ -1,17 +1,17 @@
 <?php
 
-$intval=$_POST["interval"];
-$date=$_POST["date"];
-$hour=$_POST["hour"];
+$intval=$_REQUEST["interval"];
+$date=$_REQUEST["date"];
+$hour=$_REQUEST["hour"];
 
 if ($date == "") {
 	$date = date("Y-m-d");
-	echo "from now $date <br>\n";
+	//echo "from now $date <br>\n";
 }
 if ($hour == "")
 	$hour = 0;
 $end_time = "$date $hour:00:00";
-echo $end_time."<br>";
+//echo $end_time."<br>";
 $link = mysql_connect('localhost', 'root', '');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
@@ -21,19 +21,22 @@ $db_selected = mysql_select_db('test', $link);
 if (!$db_selected) {
     die ('Can\'t use foo : ' . mysql_error());
 }
-
+//echo "intval $intval";
+//echo "date $date";
+//echo "hour $hour";
 //$result = mysql_query('SELECT * FROM temp LIMIT 0 , 30');
 //$result = mysql_query('SELECT * FROM temp ORDER BY time DESC LIMIT 0 , 60');
 $range = "WHERE temp.time > DATE_SUB('$end_time', INTERVAL 60 MINUTE)  AND temp.time <= '$end_time'";
-echo $range. "<br>";
-echo 'SELECT * FROM temp ' . $range . ' ORDER BY time DESC LIMIT 0 , 60';
-$result = mysql_query('SELECT * FROM temp ORDER BY time DESC LIMIT 0 , 60');
+//echo $range. "<br>";
+//echo 'SELECT * FROM temp ' . $range . ' ORDER BY time DESC LIMIT 0 , 60';
+$result = mysql_query('SELECT * FROM temp ' . $range . ' ORDER BY time DESC LIMIT 0 , 60');
+//$result = mysql_query('SELECT * FROM temp ORDER BY time DESC LIMIT 0 , 60');
 if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
-echo $intval."<br>";
-echo $date."<br>";
-echo $hour."<br>";
+//echo $intval."<br>";
+//echo $date."<br>";
+//echo $hour."<br>";
 $rows = array();
 while($r = mysql_fetch_assoc($result)) {
     $rows[] = $r;
